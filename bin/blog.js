b88abdcf6371922f2978eb1b7e0e41dfc6e1c6b3
Code Hz <CodeHz@outlook.com>
2016-05-03 20:31:29 +0800

    change logger

--- bin/blog.js
+++ bin/blog.js
@@ -5,7 +5,7 @@ const app = new koa();
 const config = require('../config/config.js');
 const appRouter = require('../src/router.js');
 const initDB = require('../src/init_db.js');
-const Logger = require('mini-logger');
+const logger = require('koa-logger');
 const onerror = require('koa-onerror');
 const session = require('koa-session');
 const bodyParser = require('koa-bodyparser');
@@ -14,10 +14,6 @@ const cors = require('koa-cors');
 // const router = require('koa-router');
 const json = require('koa-json');
 require('koa-qs')(app, 'first');
-const logger = Logger({
-    dir: config.logDir,
-    format: 'YYYY-MM-DD-[{category}][.log]'
-});
 app.proxy = true;
 app.use(function *(next){
     if (!this.config) this.config = config;
@@ -25,8 +21,9 @@ app.use(function *(next){
 });
 
 //router use : this.logger.error(new Error(''))
-app.context.logger = logger;
+// app.context.logger = logger;
 onerror(app);
+app.use(logger());
 app.use(session(app));
 app.use(bodyParser());
 app.use(validator());
